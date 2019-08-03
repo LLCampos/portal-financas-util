a4fc8ce14945040fb5743726a8bafcf530f0796d
LLCampos <luis.filipe.lcampos@gmail.com>
2019-08-03 09:42:42 +0100

    update iva declaratio script

--- create_last_months_periodic_iva_declaration.py
+++ create_last_months_periodic_iva_declaration.py
@@ -3,6 +3,7 @@ from time import sleep
 from datetime import datetime
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 from util import login, minus_one_month
 
@@ -30,22 +31,25 @@ def create_periodic_iva_declaration():
     today = datetime.today()
     today_last_month = minus_one_month(today)
 
-    driver.find_element_by_xpath("//select[@name = 'localizacaoSede']/option[@title = 'Continente']").click()
-    driver.find_element_by_xpath(f"//select[@name = 'anoDeclaracao']/option[@title = '{today_last_month.year}']").click()
-    driver.find_element_by_xpath(f"//select[@name = 'periodoDeclaracao']//option[@title = '{today_last_month.strftime('%m')}']").click()
-    driver.find_element_by_xpath("//select[@name = 'prazo']/option[@title = 'Dentro do prazo']").click()
+    driver.find_element_by_xpath("//lf-select[@path = 'localizacaoSede']//input").send_keys("Continente" + Keys.ENTER)
+    driver.find_element_by_xpath("//lf-select[@path = 'anoDeclaracao']//input").send_keys(f"{today_last_month.year}{Keys.ENTER}")
+    driver.find_element_by_xpath("//lf-select[@path = 'periodoDeclaracao']//input").send_keys(today_last_month.strftime('%m') + Keys.ENTER)
+    driver.find_element_by_xpath("//lf-select[@path = 'prazo']//input").send_keys("Dentro do prazo" + Keys.ENTER)
 
-    driver.find_element_by_xpath("//a[text() = ' Apuramento ']").click()
+    driver.find_element_by_xpath("//span[text() = ' Apuramento ']").click()
 
-    driver.find_element_by_xpath("//lf-radio[@lf-label = 'Tem operações em que liquidou e/ou autoliquidou imposto?']//input[@value='N']").click()
-    driver.find_element_by_xpath("//lf-radio[@lf-label = 'Tem operações em que não liquidou imposto?']//input[@value='S']").click()
-    driver.find_element_by_xpath("//lf-radio[@lf-label = 'Tem imposto dedutível e/ou regularizações?']//input[@value='N']").click()
-    driver.find_element_by_xpath("//lf-radio[@lf-catalog = 'apuramento-tem-operacoes-adquirente-com-liq-imposto']//input[@value = '02']").click()
+    sim = "0"
+    nao = "1"
+
+    driver.find_element_by_xpath(f"//lf-radio[@path = 'temOperacoesComLiqImposto']//input[@value='{nao}']/../i").click()
+    driver.find_element_by_xpath(f"//lf-radio[@path = 'temOperacoesSemLiqImposto']//input[@value='{sim}']/../i").click()
+    driver.find_element_by_xpath(f"//lf-radio[@path = 'temOperacoesDedutiveis']//input[@value='{nao}']/../i").click()
+    driver.find_element_by_xpath(f"//lf-radio[@path = 'temOperacoesAdquirenteComLiqImposto']//input[@value='{nao}']/../i").click()
 
     salary_formatted = str(salary * 100)
-    driver.find_element_by_xpath("//input[@name='btOperacoesIsentasComDeducao']").send_keys(salary_formatted)
+    driver.find_element_by_xpath("//lf-number[@path='btOperacoesIsentasComDeducao']//input").send_keys(salary_formatted)
 
-    driver.find_element_by_xpath("//button[contains(text(), 'Submeter')]").click()
+    driver.find_element_by_xpath("//span[contains(text(), 'Entregar')]").click()
 
 
 if __name__ == "__main__":
