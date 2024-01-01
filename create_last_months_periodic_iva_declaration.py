a8da952f50302d1992a55535e499bdf3ea9dc888
Luís Campos <luis.filipe.lcampos@gmail.com>
2024-01-01 13:45:10 +0000

    update code to selenium 4

--- create_last_months_periodic_iva_declaration.py
+++ create_last_months_periodic_iva_declaration.py
@@ -3,8 +3,10 @@ from datetime import datetime
 from time import sleep
 
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 from util import login, minus_one_month
 
@@ -13,48 +15,57 @@ config.read("conf.ini")
 
 
 def create_periodic_iva_declaration():
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
     driver.get(config.get("portal_financas", "url_consultar_facturas"))
     login(driver, config)
     sleep(2)
     driver.fullscreen_window()
-    tables_recibos = driver.find_elements_by_class_name("tbody-border-primary")
+    tables_recibos = driver.find_elements(By.CLASS_NAME, "tbody-border-primary")
     assert len(tables_recibos) == 1
     table = tables_recibos[0]
-    salary = int(table.find_element_by_tag_name("tr").find_elements_by_tag_name("td")[3].text.strip(" €")[:-3].replace('.', ''))
+    salary = int(
+        table.find_element(By.TAG_NAME, "tr").find_elements(By.TAG_NAME, "td")[3].text.strip(" €")[:-3].replace('.',
+                                                                                                                ''))
 
     driver.get(config.get("portal_financas", "url_declaracao_iva"))
     driver.fullscreen_window()
-    #login(driver, config)
+    # login(driver, config)
     sleep(2)
 
     today = datetime.today()
     today_last_month = minus_one_month(today)
 
-    driver.find_element_by_xpath("//lf-select[@path = 'localizacaoSede']//input").send_keys("Continente" + Keys.ENTER)
-    driver.find_element_by_xpath("//lf-select[@path = 'anoDeclaracao']//input").send_keys(f"{today_last_month.year}{Keys.ENTER}")
-    driver.find_element_by_xpath("//lf-select[@path = 'periodoDeclaracao']//input").send_keys(today_last_month.strftime('%m') + Keys.DOWN + Keys.ENTER)
-    driver.find_element_by_xpath("//lf-select[@path = 'prazo']//input").send_keys("Dentro do prazo" + Keys.ENTER)
+    driver.find_element(By.XPATH, "//lf-select[@path = 'localizacaoSede']//input").send_keys("Continente" + Keys.ENTER)
+    driver.find_element(By.XPATH, "//lf-select[@path = 'anoDeclaracao']//input").send_keys(
+        f"{today_last_month.year}{Keys.ENTER}")
+    driver.find_element(By.XPATH, "//lf-select[@path = 'periodoDeclaracao']//input").send_keys(
+        today_last_month.strftime('%m') + Keys.DOWN + Keys.ENTER)
+    driver.find_element(By.XPATH, "//lf-select[@path = 'prazo']//input").send_keys("Dentro do prazo" + Keys.ENTER)
 
-    driver.find_element_by_xpath("//span[text() = ' Apuramento ']").click()
+    driver.find_element(By.XPATH, "//span[text() = ' Apuramento ']").click()
 
     sim = "0"
     nao = "1"
 
-    driver.find_element_by_xpath(f"//lf-radio[@path = 'temOperacoesComLiqImposto']//input[@value='{nao}']/../i").click()
-    driver.find_element_by_xpath(f"//lf-radio[@path = 'temOperacoesSemLiqImposto']//input[@value='{sim}']/../i").click()
-    driver.find_element_by_xpath(f"//lf-radio[@path = 'temOperacoesDedutiveis']//input[@value='{nao}']/../i").click()
-    driver.find_element_by_xpath(f"//lf-radio[@path = 'temOperacoesAdquirenteComLiqImposto']//input[@value='{nao}']/../i").click()
+    driver.find_element(By.XPATH,
+                        f"//lf-radio[@path = 'temOperacoesComLiqImposto']//input[@value='{nao}']/../i").click()
+    driver.find_element(By.XPATH,
+                        f"//lf-radio[@path = 'temOperacoesSemLiqImposto']//input[@value='{sim}']/../i").click()
+    driver.find_element(By.XPATH, f"//lf-radio[@path = 'temOperacoesDedutiveis']//input[@value='{nao}']/../i").click()
+    driver.find_element(By.XPATH,
+                        f"//lf-radio[@path = 'temOperacoesAdquirenteComLiqImposto']//input[@value='{nao}']/../i").click()
 
     salary_formatted = str(salary * 100)
-    driver.find_element_by_xpath("//lf-number[@path='btOperacoesIsentasComDeducao']//input").send_keys(salary_formatted)
+    driver.find_element(By.XPATH, "//lf-number[@path='btOperacoesIsentasComDeducao']//input").send_keys(
+        salary_formatted)
 
-    driver.find_element_by_xpath("//*[contains(@class,'navigation-conclude')]//span[contains(text(), 'Entregar')]").click()
+    driver.find_element(By.XPATH,
+                        "//*[contains(@class,'navigation-conclude')]//span[contains(text(), 'Entregar')]").click()
     sleep(1)
-    driver.find_element_by_xpath("//*[contains(@class,'modal-footer')]//span[contains(text(), 'Entregar')]").click()
+    driver.find_element(By.XPATH, "//*[contains(@class,'modal-footer')]//span[contains(text(), 'Entregar')]").click()
     sleep(1)
-    driver.find_element_by_xpath("//span[text() = 'Autenticar']").click()
+    driver.find_element(By.XPATH, "//span[text() = 'Autenticar']").click()
     login(driver, config)
 
 
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 def minus_one_month(date):
     if date.month == 1:
         return date.replace(month=12, year=date.year-1)
@@ -6,11 +8,11 @@ def minus_one_month(date):
 
 
 def login(driver, config):
-    driver.find_elements_by_xpath('//span[text()="NIF"]')[0].click()
-    username_field = driver.find_element_by_id("username")
-    password_field = driver.find_element_by_id("password-nif")
+    driver.find_elements(By.XPATH, '//span[text()="NIF"]')[0].click()
+    username_field = driver.find_element(By.ID, "username")
+    password_field = driver.find_element(By.ID, "password-nif")
 
     username_field.send_keys(config.get("portal_financas", "nif"))
     password_field.send_keys(config.get("portal_financas", "password"))
 
-    driver.find_element_by_name("sbmtLogin").click()
+    driver.find_element(By.NAME, "sbmtLogin").click()
